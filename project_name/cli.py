7d124a0d0add8636e45c8e7493d123965b094b72
Maurice Kraus <dev@mkraus.io>
2024-03-11 13:42:34 +0100

    Add default files

---
+++ project_name/cli.py
@@ -0,0 +1,14 @@
+"""CLI interface for project_name project.
+
+"""
+
+
+def main():  # pragma: no cover
+    """
+    The main function executes on commands:
+    `python -m project_name` and `$ project_name `.
+
+    This is your program's entry point.
+    """
+
+    print("This will do something")
