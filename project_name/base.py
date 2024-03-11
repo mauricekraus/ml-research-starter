3c6a846101616fbbf798acc6571d1c069f9af4c2
Maurice Kraus <dev@mkraus.io>
2024-03-11 14:31:43 +0100

    Remove invalid docstring

--- project_name/base.py
+++ project_name/base.py
@@ -4,13 +4,6 @@ project_name base module.
 This is the principal module of the project_name project.
 here you put your main classes and objects.
 
-Be creative! do whatever you want!
-
-If you want to replace this with a Flask application run:
-
-    $ make init
-
-and then choose `flask` as template.
 """
 
 # example constant variable
